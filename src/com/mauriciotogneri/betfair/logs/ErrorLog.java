ed51f6bdbdd37258767d6a1edfec45f7fafd6308
mtogneri <mtogneri@openwt.com>
2015-08-09 21:42:14 +0200

    Not logging common errors

--- src/com/mauriciotogneri/betfair/logs/ErrorLog.java
+++ src/com/mauriciotogneri/betfair/logs/ErrorLog.java
@@ -1,10 +1,13 @@
 package com.mauriciotogneri.betfair.logs;
 
+import com.google.gson.JsonSyntaxException;
 import com.mauriciotogneri.betfair.dependency.AppObjectProvider;
+import com.mauriciotogneri.betfair.utils.StringUtils;
 import com.mauriciotogneri.betfair.utils.TimeUtils;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.SocketException;
 
 public class ErrorLog
 {
@@ -22,10 +25,33 @@ public class ErrorLog
 
     public static synchronized void log(Exception e)
     {
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(stringWriter);
-        e.printStackTrace(printWriter);
+        if (isValidException(e))
+        {
+            StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(stringWriter);
+            e.printStackTrace(printWriter);
 
-        log(stringWriter.toString());
+            log(stringWriter.toString());
+        }
+    }
+
+    private static boolean isValidException(Exception e)
+    {
+        if (e instanceof SocketException)
+        {
+            if (StringUtils.contains(e.getMessage(), "Connection reset") && StringUtils.contains(e.getMessage(), "MarketMonitor"))
+            {
+                return false;
+            }
+        }
+        else if (e instanceof JsonSyntaxException)
+        {
+            if (StringUtils.contains(e.getMessage(), "Expected BEGIN_ARRAY but was BEGIN_OBJECT") && StringUtils.contains(e.getMessage(), "MarketMonitor"))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
